6ac3be3bedcb3784b104de01e90bf4d48be4a612
Blaze34 <30avenyamadeo@gmail.com>
2014-03-18 17:42:52 +0200

    need refactor tests

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -2,9 +2,22 @@ require 'support/active_record'
 require_relative '../models/request'
 
 describe Request do
+  let(:request) { Request.new }
 
-  before(:each) do
-    @request = Request.new
+  before (:each) do
+
+  end
+
+  before (:all) do
+
+  end
+
+  context 'validation' do
+    it { request.should have(2).error_on(:url) }
+    it do
+      request.url = 'foo'
+      @request.should have(1).error_on(:url)
+    end
   end
 
   it 'requires a valid url' do
